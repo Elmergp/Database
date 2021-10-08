a9044c99697a9784a54dba9ddbda4c017104a2af
Gamboa Porras <Gamboa Porras@GAMBOAPORRAS>
2021-10-07 19:19:57 -0600

    Add project files.

---
+++ Database/exp/Store Procedures/TIdentificacionActualizar.sql
@@ -0,0 +1,34 @@
+﻿CREATE PROCEDURE [exp].[TIdentificacionActualizar]
+	@IdTipoIdentificacion int= NULL,
+	@Descripcion varchar(50)
+
+
+AS BEGIN
+SET NOCOUNT ON
+
+  BEGIN TRANSACTION TRASA
+
+    BEGIN TRY
+	
+	UPDATE EXP.TipoIdentificacion SET
+	IdTipoIdentificacion= @IdTipoIdentificacion,
+	 Descripcion=@Descripcion
+	WHERE 
+	       IdTipoIdentificacion=@IdTipoIdentificacion
+	
+	  COMMIT TRANSACTION TRASA 
+	  SELECT 0 AS CodeError, '' AS MsgError
+
+  END TRY
+
+  BEGIN CATCH
+
+	   SELECT 
+			 ERROR_NUMBER() AS CodeError,
+			 ERROR_MESSAGE() AS MsgError
+   
+	   ROLLBACK TRANSACTION TRASA
+
+   END CATCH
+
+ END
---
+++ Database/exp/Store Procedures/TIdentificacionObtener.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [exp].[TIdentificacionObtener]
+      @IdTipoIdentificacion int= NULL
+AS BEGIN
+  SET NOCOUNT ON
+
+  SELECT 
+     E.IdTipoIdentificacion,
+     E.Descripcion   
+    FROM EXP.TipoIdentificacion E
+    WHERE
+    (@IdTipoIdentificacion IS NULL OR IdTipoIdentificacion=@IdTipoIdentificacion)
+
+END
